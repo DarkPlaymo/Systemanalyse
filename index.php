016fe9a09379f9cfcda7395acb25a1ba424a22c8
GerHawX <pschumann@jobrouter.de>
2019-04-12 17:25:11 +0200

    Generalüberholung

--- index.php
+++ index.php
@@ -1,3 +1,154 @@
-<?php
-echo"Hallo Welt!";
-echo "IntelliJ ist das beste Tool der Welt!";
+<!DOCTYPE html>
+<html lang="en">
+<?php include "php/section/head.php"?>
+<body>
+<div class="preloader">
+    <img src="img/loader.gif" alt="Preloader image">
+</div>
+<?php include "php/navigation.php"?>
+<?php include "php/header.php"?>
+<section>
+    <div class="cut cut-top"></div>
+    <div class="container">
+        <div class="row intro-tables">
+            <div class="col-md-4">
+                <div class="intro-table intro-table-first">
+                    <h5 class="white heading">Today's Schedule</h5>
+                    <div class="owl-carousel owl-schedule bottom">
+                        <div class="item">
+                            <div class="schedule-row row">
+                                <div class="col-xs-6">
+                                    <h5 class="regular white">Early Exercise</h5>
+                                </div>
+                                <div class="col-xs-6 text-right">
+                                    <h5 class="white">8:30 - 10:00</h5>
+                                </div>
+                            </div>
+                            <div class="schedule-row row">
+                                <div class="col-xs-6">
+                                    <h5 class="regular white">Muscle Building</h5>
+                                </div>
+                                <div class="col-xs-6 text-right">
+                                    <h5 class="white">8:30 - 10:00</h5>
+                                </div>
+                            </div>
+                            <div class="schedule-row row">
+                                <div class="col-xs-6">
+                                    <h5 class="regular white">Cardio</h5>
+                                </div>
+                                <div class="col-xs-6 text-right">
+                                    <h5 class="white">8:30 - 10:00</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="schedule-row row">
+                                <div class="col-xs-6">
+                                    <h5 class="regular white">Early Exercise</h5>
+                                </div>
+                                <div class="col-xs-6 text-right">
+                                    <h5 class="white">8:30 - 10:00</h5>
+                                </div>
+                            </div>
+                            <div class="schedule-row row">
+                                <div class="col-xs-6">
+                                    <h5 class="regular white">Muscle Building</h5>
+                                </div>
+                                <div class="col-xs-6 text-right">
+                                    <h5 class="white">8:30 - 10:00</h5>
+                                </div>
+                            </div>
+                            <div class="schedule-row row">
+                                <div class="col-xs-6">
+                                    <h5 class="regular white">Cardio</h5>
+                                </div>
+                                <div class="col-xs-6 text-right">
+                                    <h5 class="white">8:30 - 10:00</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="schedule-row row">
+                                <div class="col-xs-6">
+                                    <h5 class="regular white">Early Exercise</h5>
+                                </div>
+                                <div class="col-xs-6 text-right">
+                                    <h5 class="white">8:30 - 10:00</h5>
+                                </div>
+                            </div>
+                            <div class="schedule-row row">
+                                <div class="col-xs-6">
+                                    <h5 class="regular white">Muscle Building</h5>
+                                </div>
+                                <div class="col-xs-6 text-right">
+                                    <h5 class="white">8:30 - 10:00</h5>
+                                </div>
+                            </div>
+                            <div class="schedule-row row">
+                                <div class="col-xs-6">
+                                    <h5 class="regular white">Cardio</h5>
+                                </div>
+                                <div class="col-xs-6 text-right">
+                                    <h5 class="white">8:30 - 10:00</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="intro-table intro-table-hover">
+                    <h5 class="white heading hide-hover">Premium Membership</h5>
+                    <div class="bottom">
+                        <h4 class="white heading small-heading no-margin regular">Register Today</h4>
+                        <h4 class="white heading small-pt">20% Discount</h4>
+                        <a href="#" class="btn btn-white-fill expand">Register</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="intro-table intro-table-third">
+                    <h5 class="white heading">Happy Clients</h5>
+                    <div class="owl-testimonials bottom">
+                        <div class="item">
+                            <h4 class="white heading content">I couldn't be more happy with the results!</h4>
+                            <h5 class="white heading light author">Adam Jordan</h5>
+                        </div>
+                        <div class="item">
+                            <h4 class="white heading content">I can't believe how much better I feel!</h4>
+                            <h5 class="white heading light author">Greg Pardon</h5>
+                        </div>
+                        <div class="item">
+                            <h4 class="white heading content">Incredible transformation and I feel so healthy!</h4>
+                            <h5 class="white heading light author">Christina Goldman</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include "php/section/services.php"?>
+<?php include "php/section/team.php"?>
+<?php include "php/section/pricing.php"?>
+<?php include "php/section/twitterSection.php"?>
+
+<?php include "php/modal.php"?>
+<?php include "php/footer.php"?>
+<!-- Holder for mobile navigation -->
+<div class="mobile-nav">
+    <ul>
+    </ul>
+    <a href="#" class="close-link"><i class="arrow_up"></i></a>
+</div>
+<!-- Scripts -->
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/typewriter.js"></script>
+<script src="js/jquery.onepagenav.js"></script>
+<script src="js/main.js"></script>
+</body>
+
+</html>
