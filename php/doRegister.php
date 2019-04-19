1a8031d124d27e606c3c6fd736756e6b16b64d80
GerHawX <pschumann@jobrouter.de>
2019-04-19 18:59:35 +0200

    Username wird bei Registrierung auf eindeutigkeit geprüft.

--- php/doRegister.php
+++ php/doRegister.php
@@ -3,11 +3,19 @@ require "psDB.php";
 $psDB = new psDB();
 $connection = $psDB->getDatabaseConnection();
 
-$query = "INSERT INTO `users`( `username`, `password`) VALUES ('".$_POST["uname"]."','".$_POST["psw"]."')";
-$psDB->doQuery($connection,$query);
+$query = "SELECT `username` FROM `users` WHERE username='" . $_POST["uname"] . "'";
+$result = $psDB->doQuery($connection, $query);
 
-if($psDB == false){
-    echo "Fehler beim Registrieren.";
-}else {
+if (sizeof($result) == 0) {
+    $query = "INSERT INTO `users`( `username`, `password`) VALUES ('" . $_POST["uname"] . "','" . $_POST["psw"] . "')";
+    $psDB->doQuery($connection, $query);
     header('location: ../index.php');
+} else {
+    echo "Username bereits vergeben!";
 }
+
+
+
+
+
+
